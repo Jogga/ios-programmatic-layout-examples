3a95ac5a49f0e8d696201632f966b884529042a9
Jogga <joachim@froestl.com>
2016-08-16 20:28:09 +0200

    add(project): xcode project setup

---
+++ ios-programmatic-layout-examples/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-programmatic-layout-examples
+//
+//  Created by Joachim Fröstl on 16.08.16.
+//  Copyright © 2016 Joachim Fröstl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
